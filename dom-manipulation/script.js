88e9c016de719b4635ad4667d18cbccf0841f841
Habiba Yvonne <habibayvonne198@gmail.com>
2025-01-28 16:06:28 +0300

    modifying the index.htm and script.js files

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -50,6 +50,17 @@ document.addEventListener('DOMContentLoaded', () => {
         ? quotes 
         : quotes.filter(quote => quote.category === selectedCategory);
     }
+    function importFromJsonFile(event) {
+        const fileReader = new FileReader();
+        fileReader.onload = function(event) {
+          const importedQuotes = JSON.parse(event.target.result);
+          quotes.push(...importedQuotes);
+          saveQuotes();
+          alert('Quotes imported successfully!');
+        };
+        fileReader.readAsText(event.target.files[0]);
+      }
+    
   
     function filterQuotes() {
       showRandomQuote();
